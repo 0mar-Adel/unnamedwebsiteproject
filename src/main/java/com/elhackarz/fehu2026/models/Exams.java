e4271f548ed0391b55b2ed33c329df61793d6554
SuchATypicalDev <120500789+SuchATypicalDev@users.noreply.github.com>
2026-05-09 16:41:34 -0300

    Update Exams.java

--- src/main/java/com/elhackarz/fehu2026/models/Exams.java
+++ src/main/java/com/elhackarz/fehu2026/models/Exams.java
@@ -2,7 +2,7 @@ package com.elhackarz.fehu2026.models;
 
 import jakarta.persistence.*;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
+@Data
 @Entity
 public class Exams {
 
@@ -12,22 +12,11 @@ public class Exams {
 
     private String examName; 
     
-    @Column(length = 500) 
+    @Column(length = 500, unique = true) 
     private String driveUrl; 
 
-    @ManyToOne 
-    @JoinColumn(name = "subject_id") 
-    @JsonIgnore 
-    private Subjects subject;
+    private String subject;
 
     public Exams() {}
 
-    
-    public Long getId() { return id; }
-    public String getExamName() { return examName; }
-    public void setExamName(String examName) { this.examName = examName; }
-    public String getDriveUrl() { return driveUrl; }
-    public void setDriveUrl(String driveUrl) { this.driveUrl = driveUrl; }
-    public Subjects getSubject() { return subject; }
-    public void setSubject(Subjects subject) { this.subject = subject; }
 }
